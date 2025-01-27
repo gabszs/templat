ac350883b91b36b10dda20928965188ae780e248
GabrielCarvalho <gabrielcarvalho.workk@gmail.com>
2025-01-26 23:11:41 -0300

    feat(Core): adding new settings and core sub-modules

--- templat/core/settings.py
+++ templat/core/settings.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic_settings import BaseSettings
 from pydantic_settings import SettingsConfigDict
 
@@ -5,9 +7,19 @@ from pydantic_settings import SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=("dev.env", ".env"), env_file_encoding="utf-8")
 
-    S3_ACCESS_ID: str
-    S3_SECRET_KEY: str
-    S3_BUCKET_URL: str
+    # S3
+    S3_ACCESS_ID: Optional[str] = None
+    S3_SECRET_KEY: Optional[str] = None
+    S3_BUCKET_URL: Optional[str] = None
+
+    # SELF AUTH
+    SECRET_KEY: Optional[str] = None
+    ALGORITHM: Optional[str] = None
+    ACCESS_TOKEN_EXPIRE_MINUTES: Optional[str] = None
+    DATETIME_FORMAT: Optional[str] = None
+
+    # THIRD AUTH
+    AUTH_SERVICE_ENDPOINT: Optional[str] = None
 
 
 settings = Settings()
